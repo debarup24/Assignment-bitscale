7c109ed7e91bc6ccf0997974b284c18ed12c8936
Debarup <87480898+debarup24@users.noreply.github.com>
2025-01-17 11:12:55 +0530

    final commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ import { BiCoinStack } from "react-icons/bi";
 
 const Sidebar = () => {
   return (
-    <div className="hidden md:hidden lg:visible px-6 pt-6 pb-4 h-[calc(100vh-65px)] gap-10 lg:flex lg:flex-col justify-between items-center border-r border-r-[#E5E5E5] max-w-14 max-lg:hidden">
+    <div className="hidden md:hidden lg:hidden xl:visible px-6 pt-6 pb-4 h-[calc(100vh-65px)] gap-10 xl:flex xl:flex-col xl:justify-between xl:items-center border-r border-r-[#E5E5E5] max-w-14 ">
       <div className="flex flex-col gap-6">
         <FiTable className="text-xl text-gray-800 cursor-pointer hover:text-pink-800" />
         <TbPuzzle className="text-xl text-gray-800 cursor-pointer hover:text-pink-800" />
